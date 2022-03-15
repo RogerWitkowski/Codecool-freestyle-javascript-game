fe9384c76acebddc6b6218c3cb749dc696f2184c
Leon Stawecki <stawecki.media@gmail.com>
2022-03-15 11:04:30 +0100

    Created start menu with hidden start, upgraded textwriter function

--- menu.js
+++ menu.js
@@ -16,20 +16,20 @@ var goBack = function(){
   };
 
 
-
-
-function typeWriter(txt,i,speed){  
+function typeWriter(txt,i,speed,paragraph){  
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById(paragraph).innerHTML += txt.charAt(i);
     i+=1;
-    setTimeout(function() {  typeWriter(txt,i,speed) }, speed);
+    setTimeout(function() {  typeWriter(txt,i,speed, paragraph) }, speed);
     }
 }
 
-function intro(){
-  document.body.innerHTML = '<p id="demo"></p>';
-  var txt = '24 February 2022 6:00';
-  var speed = 150;
+function intro(){ // if you want to add text; Create a new var with your text; duplicate the function with your variables; set the fourth parameter to demo_3 etc
+  document.body.innerHTML = '<p class="demo" id="demo_1"></p><br><p class="demo" id="demo_2"></p><br><p class="demo" id="demo_3"></p><br><p class="demo" id="demo_4"></p>';
+  var txt_1 = '24 February 2022 6:00 It\'s freezing outside...';
+  var txt_2 = 'The end of days is in sight.' 
+  var speed = 50;
   var i = 0;
-  typeWriter(txt, i, speed)
+  typeWriter(txt_1, i, speed, "demo_1")
+  let timer = setTimeout(function () {typeWriter(txt_2, i, speed, "demo_2")}, speed*txt_1.length+200)
 }
