bad9ade4339ba719af9c668c8559b7962c7a75a5
Leon Stawecki <stawecki.media@gmail.com>
2022-03-15 21:52:01 +0100

    small updated game 1

--- firstMiniGame.js
+++ firstMiniGame.js
@@ -1,7 +1,5 @@
 function questions() {
-    document.body.innerHTML = "<br><p class=\"demo\" id=\"demo_1\"></p><br>";
-    document.body.innerHTML += "<input type=\"text\" placeholder =\"Type Your name\" id=\"userinput\"><br>";
-    document.body.innerHTML += "<button type=\"submit\" id=\"submit\">click me</button>";
+    clean_page()
     txt_1 = "Volodimir: Type in your name!";
     i = 0;
     speed = 50;
@@ -9,14 +7,41 @@ function questions() {
     
     document.getElementById("submit").onclick = function(){
         var myName = document.getElementById("userinput").value;
-        myNameCheck(myName);
+        myNameCheck1(myName);
     }
 }
 
-function myNameCheck(myName){
+function clean_page(){
+    document.body.innerHTML = "<br><p class=\"demo\" id=\"demo_1\"></p><br>";
+    document.body.innerHTML += "<input type=\"text\" placeholder =\"Type Your name\" id=\"userinput\"><br>";
+    document.body.innerHTML += "<button type=\"submit\" id=\"submit\">click me</button><br>";
+}
+
+function myNameCheck1(myName){
     if (myName === "your name"){
         alert("well done")
+        yourRealName()
     } else {
-        alert("Try again!")
+        alert("You ded")
     }
+}
+
+function myNameCheck2(myName){
+    if (myName === "your real name"){
+        alert("You ded")
+    } else {
+        alert("You")
+    }
+}
+
+function yourRealName(){
+    clean_page()
+    document.getElementById("submit").onclick = function(){
+        var myName = document.getElementById("userinput").value;
+        myNameCheck2(myName);
+    }
+    txt_1 = "Volodimir: Type  now your REAL name!";
+    i = 0;
+    speed = 50;
+    typeWriter(txt_1, i, speed, "demo_1");
 }
